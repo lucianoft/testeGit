f437d435aaeec5f5918cc76dfdabc3ac48a612a1
fernandes <fernandes@DESKTOP-5QM21D0>
2017-06-11 10:59:11 -0300

    primeiro commit

---
+++ src/main/java/br/com/teste/TesteGitApplication.java
@@ -0,0 +1,12 @@
+package br.com.teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteGitApplication.class, args);
+	}
+}
